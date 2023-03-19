3d5b005298bbdf546841290750ebc2d80bd51d9f
BrineK <briancalebking@gmail.com>
2023-03-18 23:11:50 -0700

    second version final

--- app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -2,7 +2,10 @@
 
 package com.zybooks.pizzaparty
 
+//import android.os.Build.VERSION_CODES.R
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
 import android.widget.RadioGroup
@@ -11,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.ceil
 
 const val SLICES_PER_PIZZA = 8
+var stateNum = 1 //a variable to prevent infinite loop
 
 class MainActivity : AppCompatActivity() {
     //variable to store text box
@@ -22,10 +26,21 @@ class MainActivity : AppCompatActivity() {
     //variable to store radio buttons to indicate voraciousness
     private lateinit var howHungryRadioGroup: RadioGroup
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         numAttendEditText = findViewById(R.id.num_attend_edit_text)
+        numAttendEditText.addTextChangedListener(object: TextWatcher {
+         override fun afterTextChanged(s: Editable){}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int){}
+         override fun onTextChanged(s: CharSequence, start:Int,before:Int,count:Int){
+             if (stateNum == 1) {
+                 numAttendEditText.setText("")
+                 stateNum = 0
+             }
+         }
+        })
         numPizzasTextView = findViewById(R.id.num_pizzas_text_view)
         howHungryRadioGroup = findViewById(R.id.hungry_radio_group)
     }
@@ -52,5 +67,7 @@ class MainActivity : AppCompatActivity() {
 
         //output number of pizzas
         numPizzasTextView.text = "Total pizzas: $totalPizzas"
+
+        stateNum = 1
     }
 }
